aecdb8ae036d4577cc2362d99bfd34698e6f64aa
Terashima <yamanogomora@gmail.com>
2019-02-13 22:13:21 +0900

    Initial Commit

---
+++ BrowserApp2Tests/BrowserApp2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  BrowserApp2Tests.swift
+//  BrowserApp2Tests
+//
+//  Created by 寺島 洋平 on 2019/02/13.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import XCTest
+@testable import BrowserApp2
+
+class BrowserApp2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
