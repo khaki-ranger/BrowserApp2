f43c3e8fe38fe86040e0bdac63bfaec8de44a2c7
Terashima <yamanogomora@gmail.com>
2019-02-14 10:19:26 +0900

    webView にサイトを表示する処理を実装

--- BrowserApp2/ViewController.swift
+++ BrowserApp2/ViewController.swift
@@ -20,6 +20,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // View が表示された後の処理
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let urlString = "https://dotinstall.com"
+//        let urlString = ""
+        self.loadUrl(urlString: urlString)
+    }
+    
+    // アラートを表示
+    func showAlert(_ message: String) {
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // urlString が正しい値かどうかチェック
+    func getValidatedUrl(urlString: String) -> URL? {
+        if URL(string: urlString) == nil {
+            showAlert("Invalid URL")
+            return nil
+        }
+        return URL(string: urlString)
+    }
+    
+    // webView にサイトを表示
+    func loadUrl(urlString: String) {
+        if let url = self.getValidatedUrl(urlString: urlString) {
+            let urlRequest = URLRequest(url: url)
+            self.browserWebView.loadRequest(urlRequest)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
